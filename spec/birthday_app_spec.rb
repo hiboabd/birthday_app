7c84d836bd3d874ac006751350391bc32ff4fbf3
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-04-29 17:01:29 +0100

    Added text to homepage

--- spec/birthday_app_spec.rb
+++ spec/birthday_app_spec.rb
@@ -3,6 +3,15 @@ require 'spec_helper'
 feature 'testing homepage' do
   scenario 'visiting homepage and seeing correct content' do
     visit('/')
-    expect(page).to have_content "Hello World!"
+    expect(page).to have_content "Hello There!"
+    expect(page).to have_content "What is your name?"
+    expect(page).to have_content "And what is your birthday?"
   end
+
+  # scenario 'visiting homepage and using form' do
+  #   visit('/')
+  #   fill_in :name, with: 'Hibo'
+  #   click_button 'Submit'
+  #   expect(page).to have_content()
+  # end
 end
